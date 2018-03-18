e81f2358586e8113d1abe1cc51b7f8266c29f4e2
Dan Webb <d.c.webb1993@gmail.com>
2018-03-18 15:58:25 +0700

    Fix the whole top bar being a clickable link

--- src/components/top-bar/circle.js
+++ src/components/top-bar/circle.js
@@ -4,8 +4,9 @@ import color from '../../theme/color';
 const Circle = styled.div`
 	border: 3px solid ${color.white};
 	border-radius: 50%;
-	width: 30px;
+	display: inline-block;
 	height: 30px;
+	width: 30px;
 `;
 
 export default Circle;
