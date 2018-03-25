dbcf127565373e28aa4acd8c7388e2f0bd97261b
Dan Webb <d.c.webb1993@gmail.com>
2018-03-25 20:07:34 +0700

    Round bottom corners of desktop project images

--- src/components/project/mac-window/image.js
+++ src/components/project/mac-window/image.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
+import breakpoint from '../../../theme/breakpoint';
 
 const Image = styled.img`
 	display: block;
 	width: 100%;
 	height: auto;
+
+	${breakpoint('medium', `
+		border-bottom-right-radius: 4px;
+		border-bottom-left-radius: 4px;
+	`)}
 `;
 
 export default Image;
