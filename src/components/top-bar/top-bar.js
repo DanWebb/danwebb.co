530cdba3922e964b24fbcc70433aa4c06a86c207
Dan Webb <mail@danwebb.co>
2018-05-01 11:11:38 +0700

    Add journal link to top bar navigation

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Link} from 'react-static';
 import Circle from '../logo/circle';
+import {InternalLink} from '../elements/a';
 import Bar from './bar';
+import Nav from './nav';
 
 const TopBar = () => (
 	<Bar>
 		<Link to="/"><Circle/></Link>
+		<Nav>
+			<InternalLink to="/journal" color="white">Journal</InternalLink>
+		</Nav>
 	</Bar>
 );
 
